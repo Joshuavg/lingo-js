233e38a93fbf00712a9958bf8956d2b5d8e401d6
Joshua van Garderen <99046134@mydavinci.nl>
2018-01-19 09:41:17 +0100

    no message

--- game.js
+++ game.js
@@ -1,1 +1,32 @@
+var random = Math.floor( Math.random() * 479);
+var answer = words[random];
+var answerArray = answer.split('');
+console.log(answer);
 
+var checkBtn =  document.getElementById('check');
+checkBtn.setAttribute("onclick", "check();");
+
+var firstLetter = document.getElementById('letter_0_0');
+
+firstLetter.value = answerArray[0];
+var attempt = 1
+
+function check(){
+  for (var b = 0; b <= 4; b++) {
+  for (var i = 0; i <= 4; i++) {
+    var input = document.getElementById("letter_" + b + "_" + i);
+
+    console.log(input.value);
+
+    if(input.value == answerArray[i]){
+      input.style.backgroundColor = "red";
+    }
+    else
+      for (var j = 0; j <= 4; j++) {
+        if (input.value === answerArray[j]) {
+          input.style.backgroundColor = "yellow";
+        }
+      }
+    }
+  }
+}
